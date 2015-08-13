ca85dcbe8f1cca373d1ddaa3878f18dd1df69075
Florent Gouget <florent.gouget@gmail.com>
2015-08-13 17:01:43 +0200

    grid mode

--- table.js
+++ table.js
@@ -24,7 +24,7 @@
 
 var module = angular.module('90TechSAS.zl-table', []);
 
-module.directive('zlTable', ['$compile', '$timeout', function ($compile, $timeout) {
+module.directive('zlTable', ['$compile', '$timeout', '$templateCache', function($compile, $timeout, $templateCache){
 
     var ID_FIELDS = ['id', '_id', 'uid', 'uuid', '$uid'];
 
@@ -32,7 +32,7 @@ module.directive('zlTable', ['$compile', '$timeout', function ($compile, $timeou
     var rootElement;
     var tHeadAttrs, tBodyAttrs, headRowAttrs, bodyRowAttrs;
 
-    function compile(elt) {
+    function compile(elt){
         rootElement      = elt;
         var head         = _.find(elt.children(), 'tagName', 'THEAD');
         tHeadAttrs       = head.attributes;
@@ -41,25 +41,45 @@ module.directive('zlTable', ['$compile', '$timeout', function ($compile, $timeou
         availableColumns = getAvailableColumns(head, body);
         var headBuilt    = buildHeader(availableColumns);
         var bodyBuilt    = buildBody(availableColumns);
+        var bodyGrid;
+
 
         return {
-            pre: function (scope, element) {
-                $timeout(function () {
+            pre: function(scope, element){
+                $timeout(function(){
                     head.remove();
                     body.remove();
                 });
                 element.append($compile(headBuilt)(scope));
+
                 element.append($compile(bodyBuilt)(scope));
+                $timeout(function(){
+                    bodyGrid = '<div ng-if="ctrl.gridMode">' +
+                        '<div class="noselect" style="max-width:400px; float:left;margin:1%;" ng-repeat="elt in ctrl.zlTable | orderBy:ctrl.orderBy:ctrl.reverse"' +
+                        'ng-class="{\'zl-row-selected\': ctrl.isSelected(elt)}"' +
+                        'ng-click="ctrl.selectClick($event, elt)">' +
+                        '<zl-template-compiler ' +
+                        'template="' + escapeQuotes($templateCache.get(rootElement[0].attributes.getNamedItem('grid-template').value)) + '"></zl-template-compiler>' +
+                        '</div>'+
+                        '</div>' +
+                        '<div style="clear:both;"></div>';
+                    element.after($compile(bodyGrid)(scope));
+                });
+
             }
         }
     }
 
-    function getAvailableColumns(thead, tbody) {
+    function escapeQuotes(str){
+        return str.replace(/"/g, '&quot;');
+    }
+
+    function getAvailableColumns(thead, tbody){
         var row      = _.find(thead.children, 'tagName', 'TR');
         headRowAttrs = row.attributes;
         var bodyRow  = _.find(tbody.children, 'tagName', 'TR');
         bodyRowAttrs = bodyRow.attributes;
-        return _.compact(_.map(row.children, function (c, i) {
+        return _.compact(_.map(row.children, function(c, i){
             if (!c.attributes.getNamedItem('id')) return;
             return {
                 id          : c.attributes.getNamedItem('id').value,
@@ -69,29 +89,29 @@ module.directive('zlTable', ['$compile', '$timeout', function ($compile, $timeou
         }));
     }
 
-    function buildAttributes(attrs) {
-         return _.reduce(attrs, function (result, attr) {
-            if (attr.name && attr.value) {
+    function buildAttributes(attrs){
+        return _.reduce(attrs, function(result, attr){
+            if (attr.name && attr.value){
                 result += attr.name + '=' + attr.value + ' ';
             }
-             return result;
+            return result;
         }, '');
     }
 
-    function buildHeader() {
-        var elt = '<thead ' + buildAttributes(tHeadAttrs) + '>' +
-            '<tr ' + buildAttributes(headRowAttrs)+ '>' +
+    function buildHeader(){
+        var elt = '<thead ng-if="!ctrl.gridMode"' + buildAttributes(tHeadAttrs) + '>' +
+            '<tr ' + buildAttributes(headRowAttrs) + '>' +
             '<th><input type="checkbox" ng-model="selectAll" ng-click="ctrl.selectAll(selectAll)"/></th>' +
             '<th ng-repeat="col in ctrl.availableColumns | zlColumnFilter:ctrl.columns" id="{{col.id}}" ng-click="ctrl.order(col.id)" zl-drag-drop drag="col.id" drop="ctrl.dropColumn($data, col.id)">' +
             '<zl-template-compiler template="{{col.headTemplate}}"></zl-template-compiler>' +
-            '<button ng-click="ctrl.dismiss(col.id)" class="zl-table-del-btn"></button>' +
+            '&nbsp;<button ng-click="ctrl.dismiss(col.id)" class="zl-table-del-btn"></button>' +
             '</th>' +
             '</tr></thead>';
         return elt;
     }
 
-    function buildBody() {
-        var elt = '<tbody ' + buildAttributes(tBodyAttrs) + '>' +
+    function buildBody(){
+        var elt = '<tbody ng-if="!ctrl.gridMode"' + buildAttributes(tBodyAttrs) + '>' +
             '<tr ' + buildAttributes(bodyRowAttrs) + 'class="noselect" ng-repeat="elt in ctrl.zlTable | orderBy:ctrl.orderBy:ctrl.reverse" ng-click="ctrl.rowClick($event, elt)" ng-class="{\'zl-row-selected\': ctrl.isSelected(elt)}">' +
             '<td  ng-click="ctrl.selectClick($event, elt)"><input ng-click="ctrl.selectClick($event, elt); $event.stopImmediatePropagation()" type="checkbox" ng-checked="ctrl.isSelected(elt)"/></td>' +
             '<td ng-repeat="col in ctrl.availableColumns | zlColumnFilter:ctrl.columns"><zl-template-compiler template="{{col.template}}"></zl-template-compiler></td>' +
@@ -110,87 +130,89 @@ module.directive('zlTable', ['$compile', '$timeout', function ($compile, $timeou
             pagination      : '=',
             selectedData    : '=',
             selectionChanged: '&',
+            gridMode        : '=',
+            gridTemplate    : '=',
             idField         : '@'
         },
         compile         : compile,
-        controller      : function ($scope) {
+        controller      : function($scope){
             var self = this;
 
-            function dropColumn(source, target) {
+            function dropColumn(source, target){
                 var new_index = self.columns.indexOf(target);
                 var old_index = self.columns.indexOf(source);
                 self.columns.splice(new_index, 0, self.columns.splice(old_index, 1)[0]);
                 $scope.$apply();
             }
 
-            function selectAll(bool) {
-                if (bool) {
-                    self.selectedData = _.map(self.zlTable, function (el) {
+            function selectAll(bool){
+                if (bool){
+                    self.selectedData = _.map(self.zlTable, function(el){
                         return el[self.idField];
                     });
-                } else {
+                } else{
                     self.selectedData = [];
                 }
             }
 
-            function selectClick(event, elt) {
-                if (event.shiftKey && !isSelected(elt)) {
+            function selectClick(event, elt){
+                if (event.shiftKey && !isSelected(elt)){
                     rowClick(event, elt);
-                } else {
-                    if (isSelected(elt)) {
-                        _.remove(self.selectedData, function (selectedId) {
+                } else{
+                    if (isSelected(elt)){
+                        _.remove(self.selectedData, function(selectedId){
                             return selectedId === getIdValue(elt);
                         });
-                    } else {
+                    } else{
                         self.selectedData.push(getIdValue(elt));
                     }
                     self.selectionChanged({$selectedData: self.selectedData});
                 }
             }
 
-            function rowClick(event, elt) {
-                if (event.shiftKey) {
-                    if (isSelected(elt)) {
+            function rowClick(event, elt){
+                if (event.shiftKey){
+                    if (isSelected(elt)){
                         return;
                     }
                     var lastClicked = self.selectedData[self.selectedData.length - 1] || getIdValue(self.zlTable[0]);
                     var inside      = false;
-                    _.each(self.zlTable, function (currentObj) {
-                        if (getIdValue(currentObj) === lastClicked || currentObj === elt) {
+                    _.each(self.zlTable, function(currentObj){
+                        if (getIdValue(currentObj) === lastClicked || currentObj === elt){
                             inside = !inside;
                         }
-                        if (inside && !isSelected(currentObj)) {
+                        if (inside && !isSelected(currentObj)){
                             self.selectedData.push(getIdValue(currentObj));
                         }
                     });
-                    if (!isSelected(elt)) {
+                    if (!isSelected(elt)){
                         self.selectedData.push(getIdValue(elt));
                     }
                     self.selectionChanged({$selectedData: self.selectedData});
                 }
             }
 
-            function isSelected(elt) {
+            function isSelected(elt){
                 return _.includes(self.selectedData, getIdValue(elt));
             }
 
-            function getIdValue(obj) {
-                if (!self.idField) {
-                    var found = _.find(ID_FIELDS, function (f) {
+            function getIdValue(obj){
+                if (!self.idField){
+                    var found = _.find(ID_FIELDS, function(f){
                         return obj[f];
                     });
-                    if (found) {
+                    if (found){
                         self.idField = found;
                     }
                 }
                 return self.idField ? obj[self.idField] : undefined;
             }
 
-            function updateCall() {
+            function updateCall(){
                 self.update({$pagination: self.pagination});
             }
 
-            function init() {
+            function init(){
                 self.pagination             = self.pagination || {};
                 self.pagination.currentPage = self.pagination.currentPage || 0;
                 self.pagination.perPage     = self.pagination.perPage || 10;
@@ -198,17 +220,17 @@ module.directive('zlTable', ['$compile', '$timeout', function ($compile, $timeou
                 updateCall();
             }
 
-            function order(name) {
+            function order(name){
                 self.pagination.orderBy = name;
                 self.pagination.reverse = !this.reverse;
                 updateCall();
             }
 
-            function display(name) {
+            function display(name){
                 return _.contains(self.columns, name);
             }
 
-            function dismiss(name) {
+            function dismiss(name){
                 _.pull(self.columns, name);
             }
 
@@ -231,7 +253,7 @@ module.directive('zlTable', ['$compile', '$timeout', function ($compile, $timeou
 }]);
 
 
-module.directive('zlPaginate', ['$compile', '$timeout', function ($compile, $timeout) {
+module.directive('zlPaginate', ['$compile', '$timeout', function($compile, $timeout){
 
     return {
         restrict        : 'E',
@@ -244,28 +266,28 @@ module.directive('zlPaginate', ['$compile', '$timeout', function ($compile, $tim
             update    : '&',
             pagination: '='
         },
-        controller      : function () {
+        controller      : function(){
 
             var self = this;
 
-            function updateCall() {
+            function updateCall(){
                 self.update({$pagination: self.pagination});
             }
 
-            function nextPage() {
+            function nextPage(){
                 self.page(self.pagination.currentPage + 1);
             }
 
-            function previousPage() {
+            function previousPage(){
                 self.page(self.pagination.currentPage - 1);
             }
 
-            function page(pageNumber) {
+            function page(pageNumber){
                 self.pagination.currentPage = pageNumber;
                 updateCall();
             }
 
-            function paginateArray() {
+            function paginateArray(){
                 var pageNumber = Math.ceil(self.pagination.totalElements / self.pagination.perPage);
                 return new Array(isNaN(pageNumber) ? 0 : pageNumber);
             }
@@ -280,10 +302,10 @@ module.directive('zlPaginate', ['$compile', '$timeout', function ($compile, $tim
     }
 }]);
 
-module.directive('zlDragDrop', function () {
+module.directive('zlDragDrop', function(){
     return {
 
-        controller      : function () {
+        controller      : function(){
         },
         scope           : {},
         controllerAs    : 'dragDropCtrl',
@@ -291,16 +313,16 @@ module.directive('zlDragDrop', function () {
             drag: '=',
             drop: '&'
         },
-        link            : function (scope, element) {
+        link            : function(scope, element){
             var el = element[0];
 
-            if (scope.dragDropCtrl.drag) {
+            if (scope.dragDropCtrl.drag){
 
                 el.draggable = true;
 
                 el.addEventListener(
                     'dragstart',
-                    function (e) {
+                    function(e){
                         e.dataTransfer.effectAllowed = 'move';
                         e.dataTransfer.setData('Text', scope.dragDropCtrl.drag);
                         this.classList.add('drag');
@@ -311,7 +333,7 @@ module.directive('zlDragDrop', function () {
 
                 el.addEventListener(
                     'dragend',
-                    function (e) {
+                    function(e){
                         this.classList.remove('drag');
                         return false;
                     },
@@ -320,7 +342,7 @@ module.directive('zlDragDrop', function () {
 
                 el.addEventListener(
                     'dragover',
-                    function (e) {
+                    function(e){
                         e.dataTransfer.dropEffect = 'move';
                         if (e.preventDefault) e.preventDefault();
                         this.classList.add('over');
@@ -330,11 +352,11 @@ module.directive('zlDragDrop', function () {
                 );
             }
 
-            if (scope.dragDropCtrl.drop) {
+            if (scope.dragDropCtrl.drop){
 
                 el.addEventListener(
                     'drop',
-                    function (e) {
+                    function(e){
                         if (e.stopPropagation) e.stopPropagation();
                         this.classList.remove('over');
                         scope.dragDropCtrl.drop({$data: e.dataTransfer.getData('Text'), $event: e});
@@ -345,7 +367,7 @@ module.directive('zlDragDrop', function () {
 
                 el.addEventListener(
                     'dragenter',
-                    function (e) {
+                    function(e){
                         this.classList.add('over');
                         return false;
                     },
@@ -354,7 +376,7 @@ module.directive('zlDragDrop', function () {
 
                 el.addEventListener(
                     'dragleave',
-                    function (e) {
+                    function(e){
                         this.classList.remove('over');
                         return false;
                     },
@@ -365,10 +387,10 @@ module.directive('zlDragDrop', function () {
     }
 });
 
-module.directive('zlTemplateCompiler', function ($compile) {
+module.directive('zlTemplateCompiler', function($compile){
     return {
         restrict: 'E',
-        link    : function (scope, tElement, tAttrs) {
+        link    : function(scope, tElement, tAttrs){
             var template = '<div style="display:inline;">' + tAttrs.template + '</div>';
             tElement.replaceWith($compile(template)(scope));
         }
@@ -376,11 +398,11 @@ module.directive('zlTemplateCompiler', function ($compile) {
 
 });
 
-module.filter('zlColumnFilter', function () {
-    return function (allColumns, columnList) {
-        return _.sortBy(_.reject(allColumns, function (col) {
+module.filter('zlColumnFilter', function(){
+    return function(allColumns, columnList){
+        return _.sortBy(_.reject(allColumns, function(col){
             return !_.includes(columnList, col.id);
-        }), function (col) {
+        }), function(col){
             return columnList.indexOf(col.id);
         });
     }
